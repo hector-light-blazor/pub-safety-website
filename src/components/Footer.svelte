1a8bb00b5898ab13ea9e7020d036ff9b5da93be1
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2019-10-18 16:55:15 -0500

    adjustments to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -33,7 +33,10 @@
 
 
 </style>
-<footer>
+
+
+<div >
+    <footer>
     <div style="margin-left: 10%;margin-bottom: 10px;" >
         <a target="blank" href="https://www.google.com/maps/dir//1912+Joe+Stephens+Ave,+Weslaco,+TX+78599/@26.182218,-97.9743387,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x866577ee42a246bf:0x77904c368c75b699!2m2!1d-97.97215!2d26.182218">
             <img src="assets/public_safety_logo.webp" width="110" height="110" alt="E-Comms Logo">
@@ -51,3 +54,5 @@
     <div style="background: #263E70; height: 90px; width: 100%;">
     </div>
 </footer>
+
+</div>
