376d3e0b11f3d7091157aa3cb24ccc4815bee6b4
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2019-10-18 16:54:45 -0500

    added custom btn event

---
+++ src/components/ButtonNext.svelte
@@ -0,0 +1,10 @@
+<script>
+
+
+  export let lbl = "";
+  export let float = "";
+</script>
+<button on:click type="button" style="float: {float}; border-radius: 10%;cursor: pointer;background: #2D2D61; color:white;">
+    {lbl}
+
+</button>
